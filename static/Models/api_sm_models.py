6469e080b8d89c4c9b679e88bc814784ba826146
ENtelintec <136759460+ENtelintec@users.noreply.github.com>
2024-10-08 13:59:10 -0600

    update(admin-endpoints): deploy update almacen

--- static/Models/api_sm_models.py
+++ static/Models/api_sm_models.py
@@ -2,6 +2,7 @@
 __author__ = "Edisson Naula"
 __date__ = "$ 10/may./2024  at 16:31 $"
 
+
 from flask_restx import fields
 from wtforms.fields.datetime import DateField
 from wtforms.fields.list import FieldList
@@ -421,7 +422,7 @@ class SMInfoForm(Form):
     )
     comment = StringField("comment", validators=[], default="")
     destination = StringField("destination", validators=[InputRequired()])
-    history = FieldList(FormField(HistoryFormSM, "history"))
+    history = StringField("history", validators=[], default="[]")
     items = FieldList(FormField(ItemsFormSM, "items"))
     items_new = FieldList(FormField(ItemsFormSM, "items_new"))
 
