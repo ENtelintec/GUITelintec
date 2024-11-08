8be294d8ff7de5700dde0f4184201007963915c1
ENtelintec <136759460+ENtelintec@users.noreply.github.com>
2024-11-08 16:03:22 -0600

    update(almacen-gui): api resources for file download of inventory, endpoints for multiple update and insert products

--- app_gui.py
+++ app_gui.py
@@ -2,15 +2,13 @@
 __author__ = "Edisson Naula"
 __date__ = "$ 06/feb./2024  at 9:14 $"
 
-import faulthandler
-
 from templates import GUIGeneral
 from wtforms_json import init as init_wtforms_json
 
 init_wtforms_json()
 
 if __name__ == "__main__":
-    faulthandler.enable()
+    # faulthandler.enable()
     main = GUIGeneral.GUIAsistente()
     main.state("zoomed")
     main.mainloop()
