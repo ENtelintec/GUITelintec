c1d2043264a3ce4eb514e694865fee177a4297a8
ENtelintec <136759460+ENtelintec@users.noreply.github.com>
2025-09-05 17:31:04 -0600

    update(app-gui): update
    deployment

--- templates/resources/midleware/MD_SM.py
+++ templates/resources/midleware/MD_SM.py
@@ -370,11 +370,10 @@ def get_all_sm_control_table(data_token):
     abbs_list_departments, code = get_iddentifiers(data_token, ["administrator"])
     for abb in abbs_list_departments:
         dict_tabs_contracts[f"sm-{abb.lower()}-"] = abb
-    print(abbs_list_departments, code)
-    # if code != 200:
-    #     return {"data": [], "msg": iddentifiers_contracts}, 400
+    if code != 200:
+        # print(abbs_list_departments)
+        abbs_list_departments = []
     data_sm, code = get_all_sm(-1, 0, -1, with_items=False)
-    print(data_sm, code)
     if code != 200:
         return {"data": [], "msg": data_sm}, 400
     data_out = clasify_sm(
