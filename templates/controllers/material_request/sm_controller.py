ccc3d848135045b318151d53bcc554daf4f91422
ENtelintec <136759460+ENtelintec@users.noreply.github.com>
2026-02-11 10:13:53 -0600

    fix(approve_required): fix retrieve sm data

--- templates/controllers/material_request/sm_controller.py
+++ templates/controllers/material_request/sm_controller.py
@@ -242,11 +242,7 @@ def get_sm_from_item(id_item: int):
     flag, error, result = execute_sql(base_sql, val, 1)
 
     # Normalización de resultados (mismo patrón que get_sm_entries)
-    if (
-        not isinstance(result, list)
-        or not isinstance(result, tuple)
-        or len(result) == 0
-    ):
+    if len(result) == 0:
         return False, "No se encontró la SM para el id_item proporcionado", []
     return flag, error, result
 
